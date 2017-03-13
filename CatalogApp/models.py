a13ee0caf1b7b0d5ea13d4b53ad02d41700736ae
James Liu <disposedtrolley@gmail.com>
2017-03-13 19:40:05 +1100

    import ForeignKeyConstraint

--- CatalogApp/models.py
+++ CatalogApp/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, ForeignKeyConstraint
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
