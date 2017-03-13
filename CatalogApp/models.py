83e6979dbe0f92b41a7d60ac40e3a319205543a0
James Liu <disposedtrolley@gmail.com>
2017-03-13 18:51:06 +1100

    added PK constraint to category name

--- CatalogApp/models.py
+++ CatalogApp/models.py
@@ -20,7 +20,7 @@ class Category(Base):
     __tablename__ = "category"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(250), nullable=False, primary_key=True)
 
     @property
     def serialize(self):
